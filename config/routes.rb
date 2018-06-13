23afd9b4cdeeae871f999393793891da2a9df4d9
John Matthew Ian Davis <johnmidavis@hotmail.com>
2018-06-12 17:28:49 -0700

    more work on JWT devise - some actual working signup dialog

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :rumors
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static#home"
-  get "/:page" => "static#show"
+  get "/static/:page" => "static#show"
+
+  devise_for :users,
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
 end
