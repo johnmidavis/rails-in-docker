84025ffcc19eaadb294d1743225a843dc048d5e0
John Matthew Ian Davis <johnmidavis@hotmail.com>
2018-08-29 20:41:17 -0700

    some changes got abandoned - sorry

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get "/static/:page" => "static#show"
 
   devise_for :users,
+             path: "",
+             path_names:{
+                sign_in: 'login',
+                sign_out:'logout',
+                registration: 'signup'
+             },
              controllers: {
                sessions: 'sessions',
                registrations: 'registrations'
